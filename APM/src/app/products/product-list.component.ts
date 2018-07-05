4b2e341e555fe13180c8b6fb886c74fc94464c81
jesusmunozr <jesus.munozr@gmail.com>
2018-07-05 17:06:06 -0500

    Products component

---
+++ APM/src/app/products/product-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+    pageTitle = 'Product List';
+}
